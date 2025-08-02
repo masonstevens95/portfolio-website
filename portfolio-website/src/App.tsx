69bb79e178ec9ac9b4529346b75d3d43e17c760c
masonstevens95 <mason.c.stevens@gmail.com>
2025-08-02 10:28:46 -0400

    cleanup

--- portfolio-website/src/App.tsx
+++ portfolio-website/src/App.tsx
@@ -1,31 +1,11 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import { Header } from "./components/home/Header";
-import { InfiniteScrollContainer } from "./components/home/InfiniteScrollContainer";
-import { PageWrapper } from "./components/PageWrapper";
 import { HomePage } from "./pages/HomePage";
 import { YardenPage } from "./pages/YardenPage";
 import { GaribaldiPage } from "./pages/GaribaldiPage";
 import { VoiceGardenPage } from "./pages/VoiceGardenPage";
 import { VicSavePage } from "./pages/VicSavePage";
 import { HortibasePage } from "./pages/HortibasePage";
-
-const GardenPlanner = () => (
-  <PageWrapper>
-    <div className="p-10 text-white">Garden Planner Page</div>
-  </PageWrapper>
-);
-const PlantLibrary = () => (
-  <PageWrapper>
-    <div className="p-10 text-white">Plant Library Page</div>
-  </PageWrapper>
-);
-const WaterTracker = () => (
-  <PageWrapper>
-    <div className="p-10 text-white">Water Tracker Page</div>
-  </PageWrapper>
-);
-
 function App() {
   const location = useLocation();
 
