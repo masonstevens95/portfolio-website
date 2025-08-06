cb1a615a33ab66f8250e5ded275d6f463a8302a7
masonstevens95 <mason.c.stevens@gmail.com>
2025-08-06 19:16:33 -0400

    new config

---
+++ portfolio-website/src/custom-elements.d.ts
@@ -0,0 +1,9 @@
+// custom-elements.d.ts
+declare namespace JSX {
+  interface IntrinsicElements {
+    "l-waveform": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    >;
+  }
+}
